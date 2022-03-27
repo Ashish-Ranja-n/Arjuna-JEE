ea3fd8d3cd334ce97d71d02f4a86f1fa61f44f83
Ashish Ranjan <75297897+Ashish-Ranja-n@users.noreply.github.com>
2022-03-26 23:21:36 -0700

    Add files via upload

---
+++ indexjava.js
@@ -0,0 +1,21 @@
+var d = new Date().getDay();
+function random() {
+  if (d == 1) {
+             document.getElementById("r1").classList.add("day");
+                }
+  else if (d == 2) { 
+       document.getElementById("r2").classList.add("day");
+         }
+  else if (d == 3) {
+               document.getElementById("r3").classList.add("day");
+                   }
+  else if (d == 4) {
+               document.getElementById("r4").classList.add("day");
+                   }
+  else if (d == 5) {
+               document.getElementById("r5").classList.add("day");
+                   }
+  else if (d == 6) {
+               document.getElementById("r6").classList.add("day");
+                   }
+}
